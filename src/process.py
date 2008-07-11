09d410ecd59e786a41344b10b23a986269e73473
Tim Cuthbertson <tim3d.junk@gmail.com>
2008-07-11 22:42:40 +1000

    Strip out URL parameters from images

--- src/process.py
+++ src/process.py
@@ -7,9 +7,9 @@ def insert_alt_text(item):
 	"""
 	insert bolded image title text after any image on the page
 	
-	>>> item = {'soup': BeautifulSoup('<p><img src="blah" title="some texts" /></p>')}
-	>>> insert_alt_text(item)
-	{'soup': <p><img src="blah" title="some texts" /><p><b>( some texts )</b></p></p>}
+		>>> item = {'soup': BeautifulSoup('<p><img src="blah" title="some texts" /></p>')}
+		>>> insert_alt_text(item)
+		{'soup': <p><img src="blah" title="some texts" /><p><b>( some texts )</b></p></p>}
 	"""
 	soup = item['soup']
 	images = soup.findAll('img',{'title':True})
@@ -22,6 +22,25 @@ def download_images(item, dest_folder, href_prefix):
 	"""
 	Download all referenced images to the {dest} folder
 	Replace href attributes with {href_prefix}/output_filename
+	
+		>>> from lib.mock import Mock
+		>>> ensure_dir_exists = Mock()
+		>>> import process
+		>>> process.download_file = Mock()
+
+		>>> item = {'soup': BeautifulSoup('<img src="http://google.com/image.jpg?a=b&c=d"/>')}
+		>>> download_images(item, 'dest_folder', 'local_folder/')
+		{'soup': <img src="local_folder/image.jpg" />}
+	
+		# (make sure the file was downloaded from the correct URL:)
+		>>> process.download_file.call_args
+		((u'http://google.com/image.jpg?a=b&c=d', u'dest_folder/image.jpg'), {})
+		
+		# doesn't touch "dangerous" or empty file extensions
+		>>> download_images({'soup':BeautifulSoup('<img src="http://example/nasty.py"/>')}, 'dest_folder', 'local_folder/')
+		{'soup': <img src="http://example/nasty.py" />}
+		>>> download_images({'soup':BeautifulSoup('<img src="http://example/empty"/>')}, 'dest_folder', 'local_folder/')
+		{'soup': <img src="http://example/empty" />}
 	"""
 	soup = item['soup']
 	images = soup.findAll('img',{'src':True})
@@ -29,12 +48,12 @@ def download_images(item, dest_folder, href_prefix):
 		base = item['base']
 	except:
 		base = None
-
+	
 	# only accept a whitelist of image extensions for locally downloading
 	# (to prevent accidental execution of image files that look like scripts, for example)
 	safe_image_matches = ['jpe?g','gif','png','gif','bmp']
 	re_safe_image_matches = [re.compile('\.' + x + '$', re.IGNORECASE) for x in safe_image_matches]
-	images = [img for img in images if matches_any_regex(img['src'], re_safe_image_matches)]
+	images = [img for img in images if matches_any_regex(img['src'].split('?')[0], re_safe_image_matches)]
 	
 	if len(images) > 0:
 		ensure_dir_exists(dest_folder)
@@ -42,7 +61,7 @@ def download_images(item, dest_folder, href_prefix):
 		debug("Processing image link: " + img['src'])
 		href = absolute_url(img['src'], base)
 		
-		filename = get_filename(href)
+		filename = get_filename(img['src'])
 		output_filename = dest_folder + '/' + filename
 		
 		i = 2
@@ -52,6 +71,7 @@ def download_images(item, dest_folder, href_prefix):
 
 		download_file(href, output_filename)
 		img['src'] = urllib2.quote(href_prefix + filename)
+	
 	return item
 
 
@@ -64,14 +84,14 @@ def absolute_url(url, base = None):
 	"""
 	grab the absolute URL of a link that comes from {base}
 	
-	>>> absolute_url('http://abcd')
-	'http://abcd'
-	>>> absolute_url('abcd','http://google.com/stuff/file')
-	'http://google.com/stuff/abcd'
-	>>> absolute_url('abcd','http://google.com/stuff/file/')
-	'http://google.com/stuff/file/abcd'
-	>>> absolute_url('/abcd','http://google.com/stuff/file')
-	'http://google.com/abcd'
+		>>> absolute_url('http://abcd')
+		'http://abcd'
+		>>> absolute_url('abcd','http://google.com/stuff/file')
+		'http://google.com/stuff/abcd'
+		>>> absolute_url('abcd','http://google.com/stuff/file/')
+		'http://google.com/stuff/file/abcd'
+		>>> absolute_url('/abcd','http://google.com/stuff/file')
+		'http://google.com/abcd'
 	"""
 	if re.match('[a-zA-Z]+://', url):
 		return url
@@ -96,14 +116,15 @@ def get_filename(url):
 
 
 import socket
-# timeout in seconds
-socket.setdefaulttimeout(30)
 
 def download_file(url, outfile=None):
 	"""
 	Download an arbitrary URL. If outfile is given, contents are written to that file.
 	If oufile is not given, returns the contents of the file as a string.
 	"""
+	# timeout in seconds
+	socket.setdefaulttimeout(30)
+	
 	debug("downloading file: " + url)
 	dl = urllib2.urlopen(url)
 	contents = dl.read()
