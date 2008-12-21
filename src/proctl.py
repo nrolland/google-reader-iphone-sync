d8ff349b5c0ab6c55f31549910a61bc9034f5656
Tim Cuthbertson <tim3d.junk@gmail.com>
2008-12-22 01:35:12 +1100

    split out process control from main module

---
+++ src/proctl.py
@@ -0,0 +1,82 @@
+# process control
+import commands
+import signal
+
+import app_globals
+from misc import *
+
+def get_pid_filename():
+	return "%s/sync.pid" % (app_globals.OPTIONS['output_path'],)
+
+def write_pid_file(filename):
+	write_file(filename, str(os.getpid()))
+
+def get_running_pid():
+	"""
+	@throws: IOError, ValueError, RuntimeError
+	"""
+	filename =  get_pid_filename()
+	try:
+		pid = int(read_file(filename).strip())
+	except (IOError, ValueError), e:
+		e.message = ("Couldn't load PID file at %s: %s" % (filename, e.message))
+		raise
+	
+	if pid == os.getpid():
+		# it's me! it must have been stale, and happened to be reused. we don't want to kill it
+		return None
+	
+	status, output = commands.getstatusoutput("ps ux | grep -v grep | grep 'python.*GRiS' | awk '{print $2}'") # classy!
+	if output.endswith("Operation not permitted"):
+		puts(commands.getstatusoutput("uname -a"))
+		# this is hopefully just on the simulator
+		puts("Error fetching running pids: %s" % (output,))
+		puts(" - This is known to happen on the iphone simulator.")
+		puts(" - if you see it on a real device, please file a bug report")
+		status, output = (0, '') # lets just pretend it worked, and everything is fine
+		#TODO: clean
+		raise RuntimeError("Error fetching running pids: %s" % (output,))
+	if status != 0:
+		raise RuntimeError("could not execute pid-checking command. got status of %s, output:\n%s" % (status, output))
+	
+	running_pids = output.split()
+	try:
+		running_pids = [int(x) for x in running_pids if len(x) > 0]
+	except ValueError, e:
+		raise RuntimeError("one or more pids could not be converted to an integer: %r" % (running_pids,))
+	
+	if pid in running_pids:
+		return pid
+	return None
+
+def ensure_singleton_process():
+	"""
+	ensure only one sync process is ever running.
+	if --aggressive is given as a flag, this process will kill the existing one
+	otherwise, it will exit when there is already a process running
+	"""
+	aggressive = app_globals.OPTIONS['aggressive']
+	pid = None
+	try:
+		pid = get_running_pid()
+	except RuntimeError, e:
+		log_error(e, "Error fetching current PID")
+		if not aggressive:
+			sys.exit(2)
+	
+	if pid is not None:
+		if not aggressive:
+			puts("There is already a sync process running, pid=%s" % (pid,))
+			sys.exit(2)
+		else:
+			try:
+				debug("killing PID %s " %(pid,))
+				os.kill(pid, signal.SIGKILL)
+			except OSError, e:
+				msg = "couldn't kill pid %s - %s" % (pid,e)
+				puts(msg)
+				sys.exit(2)
+
+	# if we haven't exited by now, we're the new running pid!
+	filename =  get_pid_filename()
+	write_pid_file(filename)
---
+++ src/proctl_test.py
@@ -0,0 +1,129 @@
+import unittest
+import commands
+import signal
+
+from lib.mock import Mock
+import app_globals
+from misc import *
+import test_helper
+
+import proctl
+
+class GetPidTest(unittest.TestCase):
+	def setUp(self):
+		self.filename = "%s/sync.pid" % (app_globals.OPTIONS['output_path'],)
+		
+	def _mock_command(self, response):
+		commands.getstatusoutput = Mock()
+		commands.getstatusoutput.return_value = response
+		
+	def mock_pid_file(self, file_pid = '1234', command_response = (0,'1234')):
+		write_file(self.filename, file_pid)
+		self._mock_command(command_response)
+	
+	def fail(self):
+		raise RuntimeError
+		
+	def test_get_pid_should_raise_IOError_when_file_doesnt_exist(self):
+		self.mock_pid_file()
+		try:
+			os.remove(self.filename)
+		except: pass
+		self.assertRaises(IOError, proctl.get_running_pid)
+
+	def test_get_pid_should_raise_ValueError_when_file_is_not_an_integer(self):
+		self.mock_pid_file(file_pid = '')
+		self.assertRaises(ValueError, proctl.get_running_pid)
+
+	def test_get_pid_when_file_is_an_integer_with_whitespace(self):
+		self.mock_pid_file(file_pid = ' 1234 \n', command_response=(0,'1234'))
+		self.assertEqual(1234, proctl.get_running_pid())
+
+	def test_get_pid_when_pid_is_valid_and_running(self):
+		self.mock_pid_file(file_pid='1234\n', command_response=(0,'1234'))
+		self.assertEqual(1234, proctl.get_running_pid())
+		self.assertEqual(commands.getstatusoutput.call_args, (("ps ux | grep -v grep | grep 'python.*GRiS' | awk '{print $2}'",),{}))
+
+	def test_get_pid_should_return_none_when_pid_is_not_gris(self):
+		self.mock_pid_file(file_pid='1234', command_response=(0,'3456'))
+		self.assertEqual(None, proctl.get_running_pid())
+
+	def test_get_pid_should_raise_when_running_pids_cannot_be_parsed(self):
+		self.mock_pid_file(file_pid='1234', command_response=(0,'1234\nxyz'))
+		self.assertRaises(RuntimeError, proctl.get_running_pid)
+
+	def test_get_pid_should_raise_when_get_ps_command_fails(self):
+		self.mock_pid_file(file_pid='1234', command_response=(1,'3456'))
+		self.assertRaises(RuntimeError, proctl.get_running_pid)
+
+	def test_get_pid_should_return_none_when_pid_is_self(self):
+		self.mock_pid_file(file_pid=str(os.getpid()))
+		self.assertEqual(None, proctl.get_running_pid())
+
+class SingularProcessTest(GetPidTest):
+	def setUp(self):
+		super(SingularProcessTest, self).setUp()
+		self._backup_getpid = proctl.get_running_pid
+	
+	def tearDown(self):
+		proctl.get_running_pid = self._backup_getpid
+	
+	def mock_pid_process(self, aggressive=False, pid=1234, file_pid = None, command_response = (0,'')):
+		app_globals.OPTIONS['aggressive'] = aggressive
+		proctl.get_running_pid = Mock()
+		proctl.get_running_pid.return_value = pid
+
+		if file_pid is None:
+			file_pid = str(pid)
+		
+		write_file(self.filename, file_pid)
+		self._mock_command(command_response)
+		os.kill = Mock()
+
+	# aggressive ensure_singleton_process
+	def test_aggressive_singular_process_should_continue_when_kill_works(self):
+		self.mock_pid_process(aggressive = True, pid=1234, file_pid = '1234')
+		
+		proctl.ensure_singleton_process()
+		self.assertEqual(os.kill.call_args_list, [((1234, signal.SIGKILL),{})])
+		self.assertEqual(read_file(self.filename), str(os.getpid()))
+	
+	def test_aggressive_singular_process_should_exit_when_kill_fails(self):
+		self.mock_pid_process(aggressive = True, pid=1234, command_response=(0,'567\n1234\n8910'))
+		def raise_(error_cls, msg):
+			raise OSError, msg
+		os.kill = Mock(side_effect = lambda: raise_(OSError, '[Errno 3] No such process'))
+		
+		self.assertRaises(SystemExit, proctl.ensure_singleton_process)
+		self.assertEqual(os.kill.call_args_list, [((1234, signal.SIGKILL),{})])
+		self.assertEqual(read_file(self.filename), '1234') # contents should be unchanged
+		
+	def test_aggressive_singular_process_should_write_file_when_running_pids_raises(self):
+		self.mock_pid_process(aggressive = True)
+		proctl.get_running_pid = self.fail
+		proctl.ensure_singleton_process()
+		self.assertFalse(os.kill.called)
+		self.assertEqual(read_file(self.filename), str(os.getpid()))
+		
+	def test_aggressive_singular_process_should_write_pid_file_when_running_pids_returns_none(self):
+		self.mock_pid_process(aggressive = True, pid=None)
+		proctl.ensure_singleton_process()
+		self.assertFalse(os.kill.called)
+		self.assertEqual(read_file(self.filename), str(os.getpid()))
+
+	# submissive ensure_singleton_process
+	def test_submissive_singular_process_when_pid_is_valid(self):
+		self.mock_pid_process(aggressive = False, pid=1234)
+
+		self.assertRaises(SystemExit, proctl.ensure_singleton_process)
+		
+		self.assertFalse(os.kill.called)
+		self.assertEqual(read_file(self.filename), '1234') # file should be unchanged
+
+	def test_submissive_should_not_write_pid_file_when_running_pid_raises(self):
+		self.mock_pid_process(aggressive = False)
+		self.mock_pid_file(file_pid='1234')
+		proctl.get_running_pid = self.fail
+		self.assertRaises(SystemExit, proctl.ensure_singleton_process)
+		self.assertEqual(read_file(self.filename), '1234')
+		
